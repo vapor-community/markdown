cd182176e969981bfd8dbc26eb0c4ee77153af2c
Tim <0xtimc@gmail.com>
2017-01-09 19:12:48 +0000

    Remove -example target and code, this should be explained in the README and done in the tests instead.
    Using a '-' in your target causes a warning in Xcode which is annoying at best!

--- Package.swift
+++ Package.swift
@@ -2,9 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "cmark.swift",
-    targets: [
-    	Target(name: "cmark.swift-example", dependencies: ["cmark.swift"])
-    ],
     dependencies: [
     	.Package(url: "https://github.com/czechboy0/cmark.git", "0.26.1")
     ]
