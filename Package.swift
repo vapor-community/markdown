38af5ab3bde7825acdaf9213ab4703ddd31a84ca
Tim <0xtimc@gmail.com>
2017-01-27 21:40:15 +0000

    Use Broken Hands fork to remove empty example

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftMarkdown",
     dependencies: [
-    	.Package(url: "https://github.com/czechboy0/cmark.git", "0.26.1")
+    	.Package(url: "https://github.com/brokenhandsio/cmark.git", "0.26.2")
     ]
 )
