83d5715577a50e688e0be4b18a61610e132970d8
Dylan McArthur <dmcarth@users.noreply.github.com>
2017-03-27 11:06:49 -0700

    Created MarkdownOptions, added default options to markdownToHTML(_:options:)

--- Sources/cmark.swift/cmark_swift.swift
+++ Sources/cmark.swift/cmark_swift.swift
@@ -1,22 +1,37 @@
 import ccmark
 
 public enum MarkdownError: Error {
-    case conversionFailed
+	case conversionFailed
 }
 
-public func markdownToHTML(_ str: String) throws -> String {
+public struct MarkdownOptions: OptionSet {
+	public let rawValue: Int32
+	
+	public init(rawValue: Int32) {
+		self.rawValue = rawValue
+	}
+	
+	static public let sourcePosition = MarkdownOptions(rawValue: 1 << 1)
+	static public let hardBreaks = MarkdownOptions(rawValue: 1 << 2)
+	static public let safe = MarkdownOptions(rawValue: 1 << 3)
+	static public let noBreaks = MarkdownOptions(rawValue: 1 << 4)
+	static public let normalize = MarkdownOptions(rawValue: 1 << 8)
+	static public let validateUTF8 = MarkdownOptions(rawValue: 1 << 9)
+	static public let smartQuotes = MarkdownOptions(rawValue: 1 << 10)
+}
+
+public func markdownToHTML(_ str: String, options: MarkdownOptions = []) throws -> String {
 	var buffer: String?
-    try str.withCString {
-        //TODO: add options, right now passing 0
-        guard let buf = cmark_markdown_to_html($0, Int(strlen($0)), 0) else {
-        	throw MarkdownError.conversionFailed
-        }
-        buffer = String(cString: buf)
-        free(buf)
-    }
-    guard let output = buffer else { 
-        throw MarkdownError.conversionFailed
-    }
-    return output
+	try str.withCString {
+		guard let buf = cmark_markdown_to_html($0, Int(strlen($0)), options.rawValue) else {
+			throw MarkdownError.conversionFailed
+		}
+		buffer = String(cString: buf)
+		free(buf)
+	}
+	guard let output = buffer else {
+		throw MarkdownError.conversionFailed
+	}
+	return output
 }
 
