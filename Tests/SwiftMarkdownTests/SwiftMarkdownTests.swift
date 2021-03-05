bc97f5f4e8225ab3f779c723036f8c3e5cdb9831
Tim <0xtimc@gmail.com>
2021-03-05 10:48:28 +0000

    Add stress test to check for crashes and leaks

--- Tests/SwiftMarkdownTests/SwiftMarkdownTests.swift
+++ Tests/SwiftMarkdownTests/SwiftMarkdownTests.swift
@@ -35,4 +35,16 @@ class SwiftMarkdownTests: XCTestCase {
         
         XCTAssertEqual(output, expectedOutput)
     }
+
+    func testDoesntCrashUnderStress() throws {
+        let string = """
+            Turing test, created computer scientist Alan Turing, commonly discussed theme computer world. paper 1950, wanted answer question: “Can machines think?”. get bottom this, created hypothetical test see machine capable mimicking human intelligence specific conditions.
+            To conduct Turing test, researches set three separate terminals. One division operated computer, two operated humans. One human blindly question computer human specific subject. human discern answer came computer human. repeated multiple times, questioner could know answer came human half time, computer considered artificial intelligence.
+            Read [Turing Test](https://en.wikipedia.org/wiki/Turing_test)
+            """
+        for i in 1...10000 {
+            print("Iteration \(i)")
+            _ = try markdownToHTML(string)
+        }
+    }
 }
